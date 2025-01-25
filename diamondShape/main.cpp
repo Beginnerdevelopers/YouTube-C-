2886d3e622f21b01cf64cea5c4a789820d2773b8
Beginnerdevelopers <roberrtips@gmail.com>
2025-01-25 15:27:50 +0500

    diamond shape

---
+++ diamondShape/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+
+    for(int i=1;i<=n;i++){ //rows
+    cout<<endl;
+     for(int j=1; j<=n-i;j++){
+        cout<<"-";
+    }
+      for(int j=1 ; j<=2*i-1;j++){
+        cout<<"*";
+    }
+   for(int j=1; j<=n-i;j++){
+        cout<<"-";
+    }
+    // for(int j=1; j<=n-i;j++){
+    //     cout<<"-";
+    // }
+  
+    }
+    for(int i=n-1 ; i>=1 ; i--){
+    cout<<endl;
+
+        for(int j = 1;j<=n-i;j++){
+            cout<<"-";
+        }
+        for(int j =1 ; j<=2*i-1;j++){
+            cout<<"*";
+        }
+           for(int j = 1;j<=n-i;j++){
+            cout<<"-";
+        }
+    }
+    return 0 ;
+}
